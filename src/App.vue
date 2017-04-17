c689bc75178cd64f0df24072289bf2f345f7848a
zhanglihao <zhanglihao@qjdchina.com>
2017-04-17 16:30:18 +0800

    修改内容

--- src/App.vue
+++ src/App.vue
@@ -7,24 +7,5 @@
 	</div>
 </template>
 <style lang="sass" scoped>
-	.fade-enter-active, .fade-leave-active {
-		-webkit-transition: all .3s ease;
-		-webkit-transform: translateX(0px);
-	}
-	.fade-enter, .fade-leave-active {
-		-webkit-transform: translateX(-100%);
-		opacity: 0;
-	}
-	.router {
-		width: 100%;
-		/*transform: translateX(-300px);*/
-		/*position: absolute;*/
-		/*transition: all .9s cubiz-bezier(.55, 0, .1, 1);*/
-	}
-	.r {
-		width: 100%;
-		position: absolute;
-		min-height:100%;
-		z-index:2048;
-	}
+	@import './public/css/animate.scss';
 </style>
